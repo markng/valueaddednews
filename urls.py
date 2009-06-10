a2d6dc64ecf92d1ac73ae2ef537530ed6568e9c5
Mark Ng <mark@markng.co.uk>
2009-06-10 18:55:47 +0100

    static serve for images and stylesheets

--- urls.py
+++ urls.py
@@ -1,4 +1,8 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+from os import path as os_path
+import os
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,6 +22,18 @@ urlpatterns = patterns('',
         'django.views.generic.simple.direct_to_template', 
         {'template' : 'index.html'}, 
         "home",
+    ),
+    (
+        r'^images/(.*)$', 
+        'django.views.static.serve', 
+        {'document_root': os_path.join(settings.PROJECT_PATH, 'media/images')}, 
+        'images',
+    ),
+    (
+        r'^css/(.*)$', 
+        'django.views.static.serve', 
+        {'document_root': os_path.join(settings.PROJECT_PATH, 'media/css')}, 
+        'css',
     ),
     (r'^admin/', include(admin.site.urls)),
 )
