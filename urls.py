d205faaa699aac36d83b637ee26725f30593bf3a
Mark Ng <mark@markng.co.uk>
2009-06-11 11:41:18 +0100

    add admindocs in

--- urls.py
+++ urls.py
@@ -9,12 +9,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^valueaddednews/', include('valueaddednews.foo.urls')),
-
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (
