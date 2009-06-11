69727c12f00b1eda169a6e5862f407dadd1cfcfd
Mark Ng <mark@markng.co.uk>
2009-06-11 11:26:39 +0100

    add markdown, some tidying up

--- settings.py
+++ settings.py
@@ -74,6 +74,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+    'django.contrib.markup',
+    'django.contrib.admindocs',
     'cms',
     'reversion',
 )
