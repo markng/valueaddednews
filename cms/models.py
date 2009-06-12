402847be6581083617a5eba7069aa9f479344ecd
Mark Ng <mark@markng.co.uk>
2009-06-12 16:37:41 +0100

    add aside and context menu

--- cms/models.py
+++ cms/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Page(models.Model):
     """page object"""
-    slug = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, unique=True, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField(null=True, blank=True)
     published = models.BooleanField(default=False)
@@ -15,3 +15,15 @@ class Page(models.Model):
     def get_absolute_url(self):
         """docstring for get_absolute_url"""
         return '/%s' % (self.slug)
+    
+    def get_ancestors(self, ancestors=None, reverse=True):
+        """get a list of ancestor pages"""
+        if not ancestors:
+            ancestors=[]
+        if self.parent:
+            if reverse:
+                ancestors.insert(0, self.parent)
+            else:
+                ancestors.append(self.parent)
+            ancestors = self.parent.get_ancestors(ancestors=ancestors, reverse=reverse)
+        return ancestors
--- cms/views.py
+++ cms/views.py
@@ -10,7 +10,15 @@ def showpage(request, path='', template_name=None):
         raise Http404
     if not template_name:
         template_name = 'cms/standard.html' # todo - add template field to model and choose from that
+    
     responsed = {}
     responsed['page'] = page
+    
+    if page.parent:
+        # is a child of something
+        responsed['parent'] = page.parent
+        responsed['siblings'] = page.parent.children.all()
+    responsed['children'] = page.children.all()
+    
     return render_to_response(template_name, responsed)
     
