fd93ce49a7fe44e718e4db28e9812c62f5526070
Mark Ng <mark@markng.co.uk>
2009-06-26 14:43:44 +0100

    construction page

--- urls.py
+++ urls.py
@@ -11,11 +11,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    
-    # Uncomment the next line to enable the admin:
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template' : 'construction.html'}),
     (
         r'^$', 
         login_required(direct_to_template), 
